90d706fe879e9ca8cd3ec8a4259f0dc8ed63a9d4
hhussain <haarisah73@gmail.com>
2024-11-23 17:24:41 +0000

    Started adding to the flexbox

--- assets/js/My-Projects.js
+++ assets/js/My-Projects.js
@@ -18,4 +18,32 @@
    if (event.target === modalOverlay) {
      modalOverlay.style.display = 'none';
    }
+
+
+  });
+
+
+
+    // JavaScript to handle modal display
+ const showProfileBtnAscii = document.querySelector('.show-profile-btnAscii');
+ const modalOverlayAscii = document.getElementById('modalOverlayAscii');
+ const closeBtnAscii = document.getElementById('closeBtnAscii');
+
+ // Show the modal when the button is clicked
+ showProfileBtnAscii.addEventListener('click', () => {
+   modalOverlayAscii.style.display = 'flex'; // Show the modal
  });
+
+ // Hide the modal when the close button is clicked
+ closeBtnAscii.addEventListener('click', () => {
+   modalOverlayAscii.style.display = 'none'; // Hide the modal
+ });
+
+ // Hide the modal when clicking outside of it
+ modalOverlayAscii.addEventListener('click', (event) => {
+   if (event.target === modalOverlayAscii) {
+     modalOverlayAscii.style.display = 'none';
+   }
+ });
+
+
